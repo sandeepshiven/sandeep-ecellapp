c098ed965925a961ea52e07631e0d43c5a955b6d
prashant <12072000prashant@gmail.com>
2020-09-18 14:17:03 +0530

    dashboard commit from prashant

--- app/src/main/java/ecell/app/ecellteam/DashboardActivity.java
+++ app/src/main/java/ecell/app/ecellteam/DashboardActivity.java
@@ -1,20 +1,22 @@
 package ecell.app.ecellteam;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,78 +25,114 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class DashboardActivity extends AppCompatActivity {
-
-    Button button;
-    Button logout;
-    Button deptChatBtn;
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView drawer;
+    GridLayout gridLayout;
+    CardView cardView;
     FirebaseUser firebaseUser;
     FirebaseAuth mAuth;
     DatabaseReference rootRef;
     String groupName = "Full Team Chat";
-    String userId , deptName;
+    String userId, deptName, userName, email;
+    CardView deptchat, teamchat;
+    TextView name;
+    View hview;
+
+    /*--------------Activity click Handling -----------------*/
+    public void activity(View view) {
+        switch (view.getId()) {
+            case R.id.deparment:
+                Intent deptChatIntent = new Intent(DashboardActivity.this, DeptchatActivity.class);
+                deptChatIntent.putExtra("deptName", deptName);
+                deptChatIntent.putExtra("userName", userName);
+                startActivity(deptChatIntent);
+                Toast.makeText(this, "Welocome to Deparment chat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.teamchat1:
+                Toast.makeText(this, "Welocome to Teamchat", Toast.LENGTH_SHORT).show();
+                Intent fullteamchatIntent = new Intent(DashboardActivity.this, FullteamchatActivity.class);
+                fullteamchatIntent.putExtra("groupName", groupName);
+                fullteamchatIntent.putExtra("userName", userName);
+                startActivity(fullteamchatIntent);
+                break;
+            case R.id.announcement1:
+                startActivity(new Intent(DashboardActivity.this, AnnouncementActivity.class));
+                break;
+
+            case R.id.leaderboard:
+                startActivity(new Intent(DashboardActivity.this, LeaderboardActivity.class));
+                Toast.makeText(this, "Welcome to LEADERBOARD", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.work:
+                startActivity(new Intent(DashboardActivity.this, CurrentworkActivity.class));
+                Toast.makeText(this, "Welocome to Current work", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.inventory:
+                startActivity(new Intent(DashboardActivity.this, InventoryActivity.class));
+                Toast.makeText(this, "Welocome to Inventory", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.teamprofile:
+                startActivity(new Intent(DashboardActivity.this, TeamprofileActivity.class));
+                Toast.makeText(this, "Welocome to Team Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent logoutIntent = new Intent(DashboardActivity.this, LrAcitivity.class);
+                startActivity(logoutIntent);
+                Toast.makeText(this, "You Are Loged Out", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        button = (Button) findViewById(R.id.button);
-        logout = (Button) findViewById(R.id.logout);
-        deptChatBtn = (Button) findViewById(R.id.DeptChat);
+        /*--------------------------Hooks----------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer);
+        gridLayout = findViewById(R.id.gridlayout);
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
         rootRef = FirebaseDatabase.getInstance().getReference().child("Users");
-        deptChatBtn.setEnabled(false);
+        deptchat = findViewById(R.id.deparment);
+        teamchat = findViewById(R.id.teamchat1);
+        hview = navigationView.getHeaderView(0);
+        name = hview.findViewById(R.id.name);
+        deptchat.setEnabled(false);
+        teamchat.setEnabled(false);
 
+        /*-----------getting User---------------*/
         if (firebaseUser != null) {
             userId = firebaseUser.getUid();
-            String userEmail = firebaseUser.getEmail();
-        }else {
-            startActivity(new Intent(DashboardActivity.this,LrAcitivity.class));
+            email = firebaseUser.getEmail();
+        } else {
+            startActivity(new Intent(DashboardActivity.this, LrAcitivity.class));
         }
 
         getUserDept();
 
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent logoutIntent = new Intent(DashboardActivity.this, LrAcitivity.class);
-                startActivity(logoutIntent);
-                Toast.makeText(DashboardActivity.this, "Logout Successfully!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent teamChatIntent = new Intent(DashboardActivity.this,FullteamchatActivity.class);
-                teamChatIntent.putExtra("groupName",groupName);
-                startActivity(teamChatIntent);
-            }
-        });
-
-        deptChatBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent deptChatIntent = new Intent(DashboardActivity.this,DeptchatActivity.class);
-                deptChatIntent.putExtra("deptName",deptName);
-                startActivity(deptChatIntent);
-            }
-        });
+        /*---------------------------NevigationDrawer---------------------------------------*/
+        nevigationdrawer();
     }
 
 
+    /*-----------------------Getting UserDepatment--------------------*/
     private void getUserDept() {
-
         rootRef.child(userId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
                     deptName = snapshot.child("department").getValue().toString();
-                    deptChatBtn.setEnabled(true);
+                    userName = snapshot.child("name").getValue().toString();
+                    name.setText(userName + "\n" + deptName+"\n"+email);
+                    deptchat.setEnabled(true);
+                    teamchat.setEnabled(true);
                 }
             }
 
@@ -106,4 +144,60 @@ public class DashboardActivity extends AppCompatActivity {
 
     }
 
+    private void nevigationdrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+        drawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /*---------------------Drawer Item click Handling-------------*/
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.Leadership:
+                startActivity(new Intent(DashboardActivity.this, LeaderboardActivity.class));
+                Toast.makeText(this, "Welcome to LEADERBOARD", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.Logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent logoutIntent = new Intent(DashboardActivity.this, LrAcitivity.class);
+                startActivity(logoutIntent);
+                Toast.makeText(this, "You Are Loged Out", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.announcement1:
+                startActivity(new Intent(DashboardActivity.this, AnnouncementActivity.class));
+                Toast.makeText(this, "Welcome to announcement", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.teamchat1:
+                startActivity(new Intent(DashboardActivity.this, TeamprofileActivity.class));
+                Toast.makeText(this, "Welocome to Teamprofile", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
