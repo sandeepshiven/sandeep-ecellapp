776bcaabfd5b321662ed3325e0230ba750595629
sandeep shiven <sandeepshiven0@gmail.com>
2021-06-04 14:48:35 +0530

    notification service

---
+++ app/src/main/java/ecell/app/ecellteam/services/MyFirebaseInstanceService.java
@@ -0,0 +1,134 @@
+package ecell.app.ecellteam.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Random;
+
+import ecell.app.ecellteam.R;
+import ecell.app.ecellteam.SplashActivity;
+
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getData().isEmpty())
+        {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+        else
+        {
+            showNotification(remoteMessage.getData());
+        }
+    }
+
+    private void showNotification(Map<String, String> data)
+    {
+        String title = data.get("title").toString();
+        String body = data.get("body").toString();
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "ecell.app.ecellteam.services.test";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Test Check by Yash");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_focused)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, SplashActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        notificationBuilder.setContentIntent(contentIntent);
+
+
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        // Builds the notification and issues it.
+
+
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+
+    }
+
+    private void showNotification(String title, String body)
+    {
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "ecell.app.ecellteam.services.test";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Test Check by Yash");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_focused)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+        notificationManager.notify(new Random().nextInt(), notificationBuilder.build());
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, SplashActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        notificationBuilder.setContentIntent(contentIntent);
+
+
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onNewToken(@NonNull @NotNull String s) {
+        super.onNewToken(s);
+        Log.d("TOKEN", s);
+    }
+}
